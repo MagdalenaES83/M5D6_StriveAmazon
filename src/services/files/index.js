df4e4a3aa96434be9ba378f96a2bb56ee3e64d14
Crankobil <84187431+Crankobil@users.noreply.github.com>
2021-09-21 12:09:23 +0100

    Added a multiple file upload router

--- src/services/files/index.js
+++ src/services/files/index.js
@@ -1,18 +1,31 @@
 import express from 'express'
-import { writeFile} from 'fs'
+import fs from 'fs-extra'
 import multer from 'multer'
 import {join} from 'path'
+import path from 'path'
 
-
+const writeFile = fs.writeFile
 const filesRouter = express.Router()
-export const publicFolderPath = join(process.cwd(), "/public")
-const saveProductImage = (name, contentAsBuffer) => writeFile(join(publicFolderPath, name),contentAsBuffer)
+export const publicFolderPath = path.join(process.cwd(), "/public")
+const saveProductImage = (name, contentAsBuffer) => writeFile (join(publicFolderPath, name),contentAsBuffer)
+
 
 filesRouter.post("/product/:id/upload",multer().single('productImage'),async (req,res,next) => {
     try {
         console.log(req.file)
 
-        await saveProductImage(req.file.originalname, req.file.buffer)
+     await saveProductImage(req.file.originalname, req.file.buffer)
+        res.send("Uploaded!")
+    } catch (error) {
+        next(error)
+    }
+})
+
+filesRouter.post("/product/:id/uploadMultiple",multer().array('productImage'),async (req,res,next) => {
+    try {
+        console.log(req.files)
+const arrayOfPromises = req.files.map(file => saveProductImage(file.originalname, file.buffer))
+     await Promise.all(arrayOfPromises)
         res.send("Uploaded!")
     } catch (error) {
         next(error)
